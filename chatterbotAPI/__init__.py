5010069d96d77cf1f8943f603bfbba2466d16f0a
ポキ <aman_a@aol.com>
2020-10-18 13:53:34 +0500

    Changed to a Modular API

--- chatterbotAPI/__init__.py
+++ chatterbotAPI/__init__.py
@@ -3,14 +3,20 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import time
 from os import environ
+from configparser import ConfigParser
 
 DB_URL = environ.get('DB_URL', None)
 
 app = FastAPI()
+
+config_file = f"chatbot.ini"
+config = ConfigParser()
+config.read(config_file)
+
 cb = ChatBot(
     'Bot',
     storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
-    database_uri=DB_URL
+    database_uri=DB_URL if DB_URL is not None else config.get('database', 'mongo_url'),
 )
 
 trainer = ChatterBotCorpusTrainer(cb)
