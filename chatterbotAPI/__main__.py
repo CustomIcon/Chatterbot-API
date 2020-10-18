6f4f99b2eb63f6bf348d4e0762cd4cfa7452f031
ポキ <aman_a@aol.com>
2020-10-18 19:00:18 +0500

    both methods available now
    new docs

--- chatterbotAPI/__main__.py
+++ chatterbotAPI/__main__.py
@@ -3,15 +3,15 @@ import time
 from chatterbotAPI import app, cb
 
 @app.get('/')
-async def chatbot_api(query: str):
+async def chatbot_api_get(query: str):
     '''
-    `https://endpoint.mannu.me/chatbot?query={query}`
+    ``GET` https://endpoint.mannu.me/chatbot?query={query}`
 
     **Gets response from RESTFUL Chatbot-API.**\n
     **A Succesful Request would return:**\n
-    - __bot_response:__  Bot's response to the desired query\n
-    - __query:__   string user passes to the API\n
-    - __response:__   Respond code: 200 if success, else 422 if error\n
+    - __response:__   fills in the nested json with in this query\n
+    - __bot:__  Bot's response to the desired query\n
+    - __user:__   string user passes to the API\n
     - __time_taken:__ delay time for response from user to the server\n
 
 
@@ -22,15 +22,41 @@ async def chatbot_api(query: str):
     '''
     start = time.time()
     return {
-        'response': int(200),
-        'query' : query,
-        'bot_response': str(
-                cb.get_response(query)
-            ),
-        'time_taken': str(
-                f'{(round((time.time() - start)* 1000, 3))}ms'
-            )
+        'response': {
+            'user' : query,
+            'bot': str(cb.get_response(query)),
+            'time_taken': str(f'{(round((time.time() - start)* 1000, 3))}ms')
         }
+    }
+
+
+@app.post('/')
+async def chatbot_api_post(query: str):
+    '''
+    ``POST` https://endpoint.mannu.me/chatbot?query={query}`
+
+    **Gets response from RESTFUL Chatbot-API.**\n
+    **A Succesful Request would return:**\n
+    - __response:__   fills in the nested json with in this query\n
+    - __bot:__  Bot's response to the desired query\n
+    - __user:__   string user passes to the API\n
+    - __time_taken:__ delay time for response from user to the server\n
+
+
+    **Response Codes:**\n
+    - __Response__ [`200`] - Success\n
+    - __Response__ [`405`] - Method Not Allowed\n
+    - __Response__ [`422`] - Unprocessable Entity
+    '''
+    start = time.time()
+    return {
+        'response': {
+            'user' : query,
+            'bot': str(cb.get_response(query)),
+            'time_taken': str(f'{(round((time.time() - start)* 1000, 3))}ms')
+        }
+    }
+
 
 if __name__ == "__main__":
     uvicorn.run("chatterbotAPI:app", host="0.0.0.0", port=8000, log_level="info")
