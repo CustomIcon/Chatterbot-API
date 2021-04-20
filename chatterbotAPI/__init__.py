3144185921c7faab19b2ea95dc76bde4207ffc89
Akshay Rajput <thehamkercat@gmail.com>
2021-04-20 13:45:28 +0530

    Added logic adapters (#3)

--- chatterbotAPI/__init__.py
+++ chatterbotAPI/__init__.py
@@ -14,9 +14,22 @@ config_file = f"chatbot.ini"
 config = ConfigParser()
 config.read(config_file)
 
+adapters = [
+    'BestMatch',
+    'LogicAdapter',
+    'UnitConversion',
+    'TimeLogicAdapter',
+    'MathematicalEvaluation',
+    'SpecificResponseAdapter',
+]
+
+_adapters = []
+for adapter in adapters:
+    _adapters.append({'import_path', f'chatterbot.logic.{adapter}'})
 
 cb = ChatBot(
     'Bot',
+    logic_adapers=_adapters,   
     # storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
     # database_uri=DB_URL or config.get('database', 'mongo_url'),
 )
